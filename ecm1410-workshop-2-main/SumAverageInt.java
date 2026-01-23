e1af72b047d3cfde48f3770a362c0e721d3b3dea
kkb204 <kkb204@exeter.ac.uk>
2026-01-23 12:18:26 +0000

    while loop sum average

--- ecm1410-workshop-2-main/SumAverageInt.java
+++ ecm1410-workshop-2-main/SumAverageInt.java
@@ -1,20 +1,16 @@
 public class SumAverageInt {
     public static void main(String[] args) {
-        int sum = 0; 
-        double average; // The accumulated sum, init to 0
-        // average in double
+        int sum = 0;
         int lowerbound = 1;
+        double average;
         int upperbound = 100;
-        // Use a for-loop to sum from lowerbound to upperbound
-        for (int number = lowerbound; number <= upperbound; ++number) {
-            sum += number; // same as "sum = sum + number"
+        int number = lowerbound; // declare and init loop index variable
+        while (number <= upperbound) { // test
+            sum += number;
+            ++number; // update
         }
-        // Compute average in double. Beware that int/int produces int!
         int newAverage = sum / upperbound;
-
-        // Print sum and average
         System.out.println(newAverage);
         System.out.println(sum);
-
      }
 }
