e97536d1734cdf3a4b0a11b6df8460e262110ba7
kkb204 <kkb204@exeter.ac.uk>
2026-01-28 15:10:05 +0000

    complete hello world

---
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -0,0 +1,6 @@
+// HelloWorld application
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World! My name is " + args[0] + "." + " I am " + args[1] + " years old.");
+    }
+}
