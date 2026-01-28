3685957db7204c1ef8188016bc9ac5bae5d37fa9
kkb204 <kkb204@exeter.ac.uk>
2026-01-28 15:17:25 +0000

    variables test

---
+++ ecm1410-workshop-2-main/VariablesTest.java
@@ -0,0 +1,28 @@
+public class VariablesTest {
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println("x: " + x);
+        long s_per_min = 60;
+        byte s_per_day = (byte) (s_per_min * 60 * 24);
+        float area = 20.3F;
+        char a = '\u5468', b = '\\';
+        int nHours = 022;
+        double ratio = 7/5;
+        System.out.println("s_per_min: " + s_per_min);
+        System.out.println("s_per_day: " + s_per_day);
+        System.out.println("area: " + area);
+        System.out.println("a: " + a);
+        System.out.println("nHours: " + nHours);
+        System.out.println("ratio: " + ratio);
+        double d1 = 0.29/0.0001;
+        double d2 = 0.00029*1.0e7;
+        System.out.println("d1: " + d1);
+        System.out.println("d2: " + d2);
+        double epsilon = 1e-10;
+        if (Math.abs(d1 - d2) < epsilon) {
+            System.out.print("d1,d2 differ by less than ");
+            System.out.println( epsilon );
+}    
+
+    }
+}
